c334f8fcf6fde7ac604f28269df17078a85593c2
Nicolás Baier Quezada <15371725+Debaq@users.noreply.github.com>
2026-01-20 14:07:05 -0300

    cambio en direcciones para que corra en subdominios

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './app/i18n'; // Initialize i18next
@@ -8,9 +8,9 @@ import './app/i18n'; // Initialize i18next
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense fallback="Loading...">
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Suspense>
   </StrictMode>,
 )
